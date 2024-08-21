698a5274f1103789bf2600a201a33f9ca4f27b4c
Karolina Drewnik <drewnik.karolina@gmail.com>
2024-08-21 17:56:01 +0200

    Added notebooks and pdf files

---
+++ notebooks/utils.py
@@ -0,0 +1,17 @@
+COLORS_DICT = {
+    "gray": "#74787B",
+    "blue": "#1434CB",
+    "yellow": "#FCC015",
+    "dark_yellow": "#FFA600",
+}
+
+DATA_PATH = 'data'
+DATA = {
+    'raw': 'df_players.csv',
+    'processed': 'df_players_processed.csv'
+}
+
+MODELS_PATH = 'models'
+MODELS = {
+    'label_encoder': 'label_encoder.pkl'
+}
